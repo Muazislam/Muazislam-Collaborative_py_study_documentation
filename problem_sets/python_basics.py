f4bcf15313e1310f5055d417748e1a3e60dc4d06
muazislambabar <muazislambabar@gmail.com>
2026-01-25 00:52:59 +0500

    Delete problem_sets/python_basics.py

--- problem_sets/python_basics.py
+++
@@ -1,7 +0,0 @@
-############### 6.Basic Input/Output ###############
-
-# Write a program that uses input() to ask the user for their name 
-# and then prints a personalised greeting
-
-var_1 = input('Enter a valid python expression...\n')
-print(f"Hello {var_1}\nIt's nice to meet you!\nYou have a meeting today.")
