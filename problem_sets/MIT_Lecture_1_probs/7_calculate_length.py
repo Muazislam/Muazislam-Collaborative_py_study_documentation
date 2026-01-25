e4d1808f84dc6e2a8f6d8643d07584fc4720d328
muazislambabar <muazislambabar@gmail.com>
2026-01-25 20:50:13 +0500

    Add length calculation for user input phrase
    Implement a program to calculate the length of a user-input phrase.

---
+++ problem_sets/MIT_Lecture_1_probs/7_calculate_length.py
@@ -0,0 +1,4 @@
+# Calculate Length: Ask the user to input a phrase and use the built-in len() function to print the number of characters in the input
+
+phrase = input("Please enter a phrase....\n ")
+print(len(phrase))
