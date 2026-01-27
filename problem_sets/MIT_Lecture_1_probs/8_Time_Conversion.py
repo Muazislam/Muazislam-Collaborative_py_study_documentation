eee737095b8340818e96a87ca76654dbb6e813eb
muazislambabar <muazislambabar@gmail.com>
2026-01-27 10:41:53 +0500

    Create 8_Time_Conversion.py

---
+++ problem_sets/MIT_Lecture_1_probs/8_Time_Conversion.py
@@ -0,0 +1,5 @@
+# 8 Time Conversion: Write expressions to calculate the total number of 
+# seconds contained in 42 minutes and 42 seconds.
+
+seconds = (42 * 60) + 42
+print(f"The total number of seconds is {seconds}")
