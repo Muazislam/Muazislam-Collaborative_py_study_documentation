05af1023dc0bcb04bc2a7f4c841db280bb1210a9
muazislambabar <muazislambabar@gmail.com>
2026-02-01 08:32:02 +0500

    Create 4_string_arithmetic.py for string operations
    Add a script to demonstrate string arithmetic in Python.

---
+++ problem_sets/MIT_Lecture_1_probs/4_string_arithmetic.py
@@ -0,0 +1,9 @@
+## 4: String Arithmetic: Determine what the following expressions evaluate to: 'spam' + 'spamspam' and 'spam' * 3
+
+print('spam' + 'spamspam')
+# In python, concatination among strings happens by placing a +ive sign between them.
+# Concationation is the process of merging two strings together.
+print('spam' * 3)
+# In python, when a string is multiplied by an integer, the string is written repeatedly as many times as the highet of the digit.
+# The string after multiplication is written on  single line and 
+# all the multiple strings are written jointly between the same '' colomns.
