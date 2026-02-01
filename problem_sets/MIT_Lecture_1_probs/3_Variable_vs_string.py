2e229f6405055ebad90b206262747787ad16de31
muazislambabar <muazislambabar@gmail.com>
2026-02-01 08:30:23 +0500

    Create 3_Variable_vs_string.py to illustrate variables
    Added a script to demonstrate the difference between a variable and a string.

---
+++ problem_sets/MIT_Lecture_1_probs/3_Variable_vs_string.py
@@ -0,0 +1,9 @@
+## 3: Variable vs. String: Determine which of the following is a variable name and which is a string: spam and 'spam'.
+
+spam = 'I am a variable';
+
+# If we have a variable, it will print a value.
+# In case the variable is empty, it will crash the code.
+# For a string, it will print itself the way it should.
+print(spam)
+print('spam')
